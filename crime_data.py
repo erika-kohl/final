9ac2d59a8897b0d162ab6ef68dd03bb7a6d2aa80
zzemper <zzemper@umich.edu>
2020-12-06 17:06:49 -0500

    crime_data fixed up, collecting 25 rows at a time

--- crime_data.py
+++ crime_data.py
@@ -18,7 +18,33 @@ response = requests.get("https://api.usa.gov/crime/fbi/sapi/api/arrest/states/of
 #print(response.status_code)
 #print(response.json())
 
-def get_city_list(cur, conn):
+def create_database(db_file):
+    '''
+    This function creates the database which will be used to store all the data
+    '''
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_file)
+    cur = conn.cursor()
+    return cur, conn
+
+def create_state_table(cur, conn):
+    '''
+    This function creates the States lookup table and adds it to the database.It sets the abbreviation for each state as the primary key since each is unique."
+    '''
+    
+    states = ["Alabama","Alaska", "Arizona","Arkansas","California","Colorado","Connecticut","District of Columbia","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
+    state_abr = ["AL","AK","AZ","AR","CA","CO","CT", "DC","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+    
+    cur.execute("CREATE TABLE IF NOT EXISTS States (id TEXT PRIMARY KEY, title TEXT)")
+
+    cur.execute('SELECT COUNT(*) FROM States')
+    row_count = cur.fetchone()[0]
+    if row_count == 0:
+        for i in range(len(states)):
+            cur.execute("INSERT INTO States (id,title) VALUES (?,?)",(state_abr[i],states[i]))
+        conn.commit() 
+
+def create_dangerous_cities_table(cur, conn):
     '''
     This function uses a website to webscrape the names of the top 100 most dangerous US cities. 
     Source: https://www.neighborhoodscout.com/blog/top100dangerous
@@ -43,52 +69,38 @@ def get_city_list(cur, conn):
         cityList.append(city)
         state = separated_location[1]
         stateList.append(state)
-    
-    print(cityList)
-    print(stateList)
 
     cur.execute("CREATE TABLE IF NOT EXISTS Dangerous_Cities (id INTEGER PRIMARY KEY, city TEXT, state TEXT)")
-    for i in range((stateList)):
-        cur.execute("INSERT INTO States (id,city,state) VALUES (?,?,?)",(i,cityList[i],stateList[i]))
+    '''
+    for i in range(len(stateList)):
+        cur.execute("INSERT INTO Dangerous_Cities (id,city,state) VALUES (?,?,?)",(i,cityList[i],stateList[i]))
     conn.commit() 
-
-
-def create_database(db_file):
     '''
-    This function creates the database which will be used to the data
-    '''
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db_file)
-    cur = conn.cursor()
-    return cur, conn
-
-def create_state_table(cur, conn):
-    '''
-    This function creates the States lookup table and adds it to the database.It sets the abbreviation for each state as the primary key since each is unique."
-    '''
-    
-    states = ["Alabama","Alaska", "Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","IllinoisIndiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","MontanaNebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","PennsylvaniaRhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
-    state_abr = ["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
-    
-    cur.execute("CREATE TABLE IF NOT EXISTS States (id TEXT PRIMARY KEY, title TEXT)")
-    for i in range(len(states)):
-        cur.execute("INSERT INTO States (id,title) VALUES (?,?)",(state_abr[i],states[i]))
-    conn.commit() 
-
-
-def json_print():
-    # create a formatted string of the Python JSON object
-    obj = response.json()
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
+    # want the loop to run 25 times
+    cur.execute('SELECT COUNT(*) FROM Dangerous_Cities')
+    row_count = cur.fetchone()[0]
+    if row_count == 0:
+        for i in range(25):
+            cur.execute("INSERT INTO Dangerous_Cities (id,city,state) VALUES (?,?,?)",(i,cityList[i],stateList[i]))
+        conn.commit() 
+    elif row_count == 25:
+        for i in range(25, 50):
+            cur.execute("INSERT INTO Dangerous_Cities (id,city,state) VALUES (?,?,?)",(i,cityList[i],stateList[i]))
+        conn.commit() 
+    elif row_count == 50:
+        for i in range(50, 75):
+            cur.execute("INSERT INTO Dangerous_Cities (id,city,state) VALUES (?,?,?)",(i,cityList[i],stateList[i]))
+        conn.commit() 
+    elif row_count == 75:
+        for i in range(75, 100):
+            cur.execute("INSERT INTO Dangerous_Cities (id,city,state) VALUES (?,?,?)",(i,cityList[i],stateList[i]))
+        conn.commit() 
 
 def main():
     # SETUP DATABASE AND TABLE
     cur, conn = create_database('crime.db')
     create_state_table(cur, conn)
-    get_city_list(cur, conn)
-    json_print()
+    create_dangerous_cities_table(cur, conn)
 
     
 if __name__ == "__main__":
