2d3887a0c91ab13a82831e55633ab9e5f970afa0
Erika Kohl <ekohl@umich.edu>
2020-12-06 12:43:47 -0500

    Adding Dangerous Cities to DB

--- crime_data.py
+++ crime_data.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 #API_key = "43vyNBaRamuvatb61ifsdblzCXp6qmkYz2ZJs9Hs"
 #API 3 - Gets Crime counts by category based on State
-#response = requests.get("https://api.usa.gov/crime/fbi/sapi/api/arrest/states/offense/IL/all/2017/2019?API_KEY=43vyNBaRamuvatb61ifsdblzCXp6qmkYz2ZJs9Hs")
+response = requests.get("https://api.usa.gov/crime/fbi/sapi/api/arrest/states/offense/IL/all/2017/2019?API_KEY=43vyNBaRamuvatb61ifsdblzCXp6qmkYz2ZJs9Hs")
 
 #Extra API -  Gets State population by year
 #response = requests.get("https://datausa.io/api/data?drilldowns=State&measures=Population&year=2016")
@@ -18,7 +18,7 @@ from bs4 import BeautifulSoup
 #print(response.status_code)
 #print(response.json())
 
-def get_city_list():
+def get_city_list(cur, conn):
     '''
     This function uses a website to webscrape the names of the top 100 most dangerous US cities. 
     Source: https://www.neighborhoodscout.com/blog/top100dangerous
@@ -47,7 +47,10 @@ def get_city_list():
     print(cityList)
     print(stateList)
 
-
+    cur.execute("CREATE TABLE IF NOT EXISTS Dangerous_Cities (id INTEGER PRIMARY KEY, city TEXT, state TEXT)")
+    for i in range((stateList)):
+        cur.execute("INSERT INTO States (id,city,state) VALUES (?,?,?)",(i,cityList[i],stateList[i]))
+    conn.commit() 
 
 
 def create_database(db_file):
@@ -70,45 +73,22 @@ def create_state_table(cur, conn):
     cur.execute("CREATE TABLE IF NOT EXISTS States (id TEXT PRIMARY KEY, title TEXT)")
     for i in range(len(states)):
         cur.execute("INSERT INTO States (id,title) VALUES (?,?)",(state_abr[i],states[i]))
-    conn.commit()
+    conn.commit() 
 
-
-def create_state_demo():
-    '''
-    '''
-    pass
-
- 
- 
 
 def json_print():
     # create a formatted string of the Python JSON object
     obj = response.json()
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
-    
-    #ith open('nyt_data.txt', 'w') as outfile:  
-       #json.dump(text, outfile)
-       # print("opened")
-    
-    #with open('nyt_data.txt', 'w') as f:
-        #json.dump(text, f)
-    
-    #with open("nyt_data", 'w', newline='') as file:
-        #writer = csv.writer(file)     
-        #write column titles
-        #writer.writerow(['Critic', 'Review', 'Movie'])
-        #write data
-        #for rows in text:
-            #print(rows)
-            #writer.writerow(tuple(rows))
+
 
 def main():
     # SETUP DATABASE AND TABLE
     cur, conn = create_database('crime.db')
     create_state_table(cur, conn)
-    get_city_list()
-    #json_print()
+    get_city_list(cur, conn)
+    json_print()
 
     
 if __name__ == "__main__":
