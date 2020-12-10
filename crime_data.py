949c81016e3f3938d6b22c37e20d94f9a3478be1
Erika Kohl <ekohl@umich.edu>
2020-12-10 10:51:53 -0500

    webscrapes the names and abbreviations of the US states- now runs 7 times, 3 for states, 4 for cities

--- crime_data.py
+++ crime_data.py
@@ -4,6 +4,7 @@ import sqlite3
 import os
 import csv
 from bs4 import BeautifulSoup
+import regex as re
 
 def create_database(db_file):
     '''
@@ -14,24 +15,46 @@ def create_database(db_file):
     cur = conn.cursor()
     return cur, conn
 
-def create_state_table(cur, conn):
+def create_state_table(cur,conn,start,end):
     '''
-    This function creates the States lookup table and adds it to the database.
+    This function creates the States lookup table and adds it to the database. 
+    It uses Beautifulsoup to find the state names, and regex to get the abbrevaition.
     It assigns the state to a unique primary key id, inserts the abbreviation for 
     each state, and inserts the full name of the state."
     '''
-    
-    states = ["Alabama","Alaska", "Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Colombia","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
-    state_abr = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
-    
-    cur.execute("CREATE TABLE IF NOT EXISTS States (id INTEGER PRIMARY KEY, abbreviation TEXT , state_name TEXT)")
+    state = str()
+    states = []
+    #one request each time it is ran
+    state_list_URL = "https://www.safehome.org/safest-cities/"
+    page = requests.get(state_list_URL) 
+    soup = BeautifulSoup(page.content, 'html.parser')
+    tags = soup.find("ul" , class_="home-state-list list-unstyled")
 
-    cur.execute('SELECT COUNT(*) FROM States')
-    row_count = cur.fetchone()[0]
-    if row_count == 0:
-        for i in range(len(states)):
-            cur.execute("INSERT INTO States (id,abbreviation,state_name) VALUES (?,?,?)",(i,state_abr[i],states[i]))
-        conn.commit() 
+    #get state names
+    for state in tags:
+        state_name = state.text
+        states.append(state_name)
+    
+    #get state abbreviations
+    state_abr = []
+    pattern = r'cities\/(\w\w)'
+    for line in tags:
+        new_line = str(line)
+        matches =  re.findall(pattern, new_line)
+        for match in matches:
+            state_abr.append(match.upper())
+    
+    #insert missing locations 
+    states.insert(8, "District of Colombia")
+    state_abr.insert(8, "DC")
+    states.insert(11, "Hawaii")
+    state_abr.insert(11, "HI")
+    states.insert(13, "Illinois")
+    state_abr.insert(13, "IL")
+    
+    for i in range(start,end):
+        cur.execute("INSERT INTO States (id,abbreviation,state_name) VALUES (?,?,?)",(i,state_abr[i],states[i]))
+    conn.commit() 
 
 def create_dangerous_cities_table(cur,conn,start,end):
     '''
@@ -42,15 +65,16 @@ def create_dangerous_cities_table(cur,conn,start,end):
     '''
     city = str()
     city_list = []
-    city_list_URL = "https://www.safehome.org/safest-cities/"
+    #one request each time it is ran
+    city_list_URL = "https://www.neighborhoodscout.com/blog/top100dangerous"
     page = requests.get(city_list_URL) 
     soup = BeautifulSoup(page.content, 'html.parser')
     tags = soup.find_all("h3")
     for location in tags:
-        city_state = location.text
+        title = location.find("a")
+        city_state = title.text
         city_list.append(city_state)
-        if len(city_list) >= 100:
-            break
+
     cityList = []
     stateList = []
     #separate city from state
@@ -78,6 +102,7 @@ def create_safe_cities_table(cur, conn, start, end):
     '''
     city = str()
     city_list = []
+    #one request each time it is ran
     city_list_URL = "https://www.safehome.org/safest-cities/"
     page = requests.get(city_list_URL) 
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -108,48 +133,67 @@ def create_safe_cities_table(cur, conn, start, end):
 def main():
     #setup database
     cur, conn = create_database('crime.db')
+    
     #create State table if not exists
-    create_state_table(cur, conn)
+    cur.execute("CREATE TABLE IF NOT EXISTS States (id INTEGER PRIMARY KEY, abbreviation TEXT , state_name TEXT)")
 
-    #create Safe_Cities table if not exists
-    cur.execute("CREATE TABLE IF NOT EXISTS Safe_Cities (id INTEGER PRIMARY KEY, city TEXT, state_id INTEGER)")
-    #create Dangerous_Cities table if not exists
-    cur.execute("CREATE TABLE IF NOT EXISTS Dangerous_Cities (id INTEGER PRIMARY KEY, city TEXT, state_id INTEGER)")
+    cur.execute('SELECT COUNT(*) FROM States')
+    state_row_count = cur.fetchone()[0]
     
-    #limiting amount of data collected to 25 rows each time file is ran
-    cur.execute('SELECT COUNT(*) FROM Safe_Cities')
-    safe_row_count = cur.fetchone()[0]
-
-    cur.execute('SELECT COUNT(*) FROM Dangerous_Cities')
-    danger_row_count = cur.fetchone()[0]
+    if state_row_count == 0:
+        print("Collecting States and State Abbreviations...(1/3)")
+        create_state_table(cur,conn,0,22)
+        print("Finished")
     
-    if safe_row_count == 0 and danger_row_count == 0:
-        print("Collecting Web Data...(1/4)")
-        create_safe_cities_table(cur,conn,0,25)
-        create_dangerous_cities_table(cur,conn,0,25)
+    elif state_row_count == 22:
+        print("Collecting States and State Abbreviations...(2/3)")
+        create_state_table(cur,conn,22,40)
         print("Finished")
-
-    elif safe_row_count == 25 and danger_row_count == 25:
-        print("Collecting Web Data...(2/4)")
-        create_safe_cities_table(cur,conn,25,50)
-        create_dangerous_cities_table(cur,conn,25,50)
-        print("Finished")
-
-    elif safe_row_count == 50 and danger_row_count == 50:
-        print("Collecting Web Data...(3/4)")
-        create_safe_cities_table(cur,conn,50,75)
-        create_dangerous_cities_table(cur,conn,50,75)
-        print("Finished")
-
-    elif safe_row_count == 75 and danger_row_count == 75:
-        print("Collecting Web Data...(4/4)")
-        create_safe_cities_table(cur,conn,75,100)
-        create_dangerous_cities_table(cur,conn,75,100)
-        print("Finished")
-
-    elif safe_row_count == 100 and danger_row_count == 100:
-        print("All 100 rows of Web Data have been inserted into the Safe_Cities and Dangerous_Cities tables in the database.")
-
     
+    elif state_row_count == 40:
+        print("Collecting States and State Abbreviations...(3/3)")
+        create_state_table(cur,conn,40,51)
+        print("States table is Completed.")
+
+    if state_row_count == 51:
+        #create Safe_Cities table if not exists
+        cur.execute("CREATE TABLE IF NOT EXISTS Safe_Cities (id INTEGER PRIMARY KEY, city TEXT, state_id INTEGER)")
+        #create Dangerous_Cities table if not exists
+        cur.execute("CREATE TABLE IF NOT EXISTS Dangerous_Cities (id INTEGER PRIMARY KEY, city TEXT, state_id INTEGER)")
+    
+        #limiting amount of data collected to 25 rows each time file is ran
+        cur.execute('SELECT COUNT(*) FROM Safe_Cities')
+        safe_row_count = cur.fetchone()[0]
+
+        cur.execute('SELECT COUNT(*) FROM Dangerous_Cities')
+        danger_row_count = cur.fetchone()[0]
+    
+        if safe_row_count == 0 and danger_row_count == 0:
+            print("Collecting Web Data...(1/4)")
+            create_safe_cities_table(cur,conn,0,25)
+            create_dangerous_cities_table(cur,conn,0,25)
+            print("Finished")
+
+        elif safe_row_count == 25 and danger_row_count == 25:
+            print("Collecting Web Data...(2/4)")
+            create_safe_cities_table(cur,conn,25,50)
+            create_dangerous_cities_table(cur,conn,25,50)
+            print("Finished")
+
+        elif safe_row_count == 50 and danger_row_count == 50:
+            print("Collecting Web Data...(3/4)")
+            create_safe_cities_table(cur,conn,50,75)
+            create_dangerous_cities_table(cur,conn,50,75)
+            print("Finished")
+
+        elif safe_row_count == 75 and danger_row_count == 75:
+            print("Collecting Web Data...(4/4)")
+            create_safe_cities_table(cur,conn,75,100)
+            create_dangerous_cities_table(cur,conn,75,100)
+            print("Safe_Cities and Dangerous_Cities tables are Completed.")
+
+        elif safe_row_count == 100 and danger_row_count == 100:
+            print("All 100 rows of Web Data have been inserted into the Safe_Cities and Dangerous_Cities tables in the database.")
+ 
 if __name__ == "__main__":
     main()
